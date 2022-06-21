9d2585ab6b2d7492120f8e703b7457037b0e9852
Nikola M <nikola.merlic2@gmail.com>
2022-06-21 17:08:12 +0200

    Dodaja basic post/get

--- src/db.js
+++ src/db.js
@@ -1,44 +1,23 @@
 import mongo from "mongodb";
 
+
     let connection_string = "mongodb://localhost:27017/";
+   
     let client = new mongo.MongoClient(connection_string, {
         useNewUrlParser: true,
          useUnifiedTopology: true });
 
+    let db = null;
+    
+    function isConnected(){
+        return !!client && !!client.topology && client.topology.isConnected
+    }
 
-         let db = null
-
-        export default () =>{
-        return new Promise((resolve, reject) =>{
-
-           if (db && client.isConnected()) {
-            resolve(db)
-           }     
-
-            client.connect(err =>{
-                if(err) {
-                    reject("Doslo je do greske prilikom spajanja" + err)
-                }
-                else {
-                    console.log("Uspjesno spajanje na bazu")
-                    let db = client.db("petfinder")
-                    resolve(db)
-                }
-            })
-        })
+    export default async () =>{
+        if (!db || !isConnected()) {
+            await client.connect();
+            db = client.db("petfinder");
+            console.log("Connected OK");
         }
-
-/*
-client.connect(err => {
- if (err) {
- console.error(err);
- return;
- }
- console.log("Database connected successfully!");
-
- // za sada ništa nećemo raditi, samo zatvaramo pristup sljedećom naredbom
- client.close();
-});
-
-*/
-
+        return db;
+    }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
+
+import connect from "./db.js"
 
 const app = express()
 const port = 3001
@@ -10,8 +12,35 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+
+let counter = 0;
+
+app.get("/pronadiljubimca", async (req, res) =>{
+    console.log(++counter);
+    let db = await connect();
+    let kolekcija = db.collection("pronadiljubimca");
+    let cursor = await kolekcija.find();
+    let data = await cursor.toArray();
+
+    res.json(data);
+});
+
+
+app.post("/prijavanestanka", async (req,res) => {
+    let doc = req.body;
+    console.log(doc);
+
+    let db = await connect();
+    let kolekcija = db.collection("prijavanestanka");
+
+    let result = await kolekcija.insertOne(doc)
+
+    res.status(201);
+    res.send();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
