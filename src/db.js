b1ee842669a267fbc1b90a7e0383386f40454150
Meks12 <nikola.merlic2@gmail.com>
2022-07-25 21:19:53 +0200

    typo

--- src/db.js
+++ src/db.js
@@ -1,24 +1,26 @@
 import mongo from "mongodb";
 
-// spajanje lokalno na mongodb
+// spajanje na mongo
 
-    let connection_string = "mongodb+srv://admin:admin@cluster0.upjf8.mongodb.net/test/";
-   
-    let client = new mongo.MongoClient(connection_string, {
-        useNewUrlParser: true,
-         useUnifiedTopology: true });
+let connection_string =
+  "mongodb+srv://admin:admin@cluster0.upjf8.mongodb.net/test/";
 
-    let db = null;
-    
-    function isConnected(){
-        return !!client && !!client.topology && client.topology.isConnected
-    }
+let client = new mongo.MongoClient(connection_string, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-    export default async () =>{
-        if (!db || !isConnected()) {
-            await client.connect();
-            db = client.db("pfinder");
-            console.log("Connected OK");
-        }
-        return db;
-    }
+let db = null;
+
+function isConnected() {
+  return !!client && !!client.topology && client.topology.isConnected;
+}
+
+export default async () => {
+  if (!db || !isConnected()) {
+    await client.connect();
+    db = client.db("pfinder");
+    console.log("Connected OK");
+  }
+  return db;
+};
