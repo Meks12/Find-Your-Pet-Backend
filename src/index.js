81cfafe0a512e24c24678ad83250060a973e0f0c
Meks12 <nikola.merlic2@gmail.com>
2022-08-31 20:14:28 +0200

    commit za deploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connect from "./db.js";
 import mongo from "mongodb";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
